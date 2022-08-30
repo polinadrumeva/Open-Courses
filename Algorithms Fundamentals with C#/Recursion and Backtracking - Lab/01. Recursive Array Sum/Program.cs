9e97fafa2a0cad7139a71aaff50ba4969d415681
Polina <polina9130@gmail.com>
2022-08-30 16:55:47 +0300

    add recursion problems

---
+++ Algorithms Fundamentals with C#/Recursion and Backtracking - Lab/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Recursive_Array_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Algorithms Fundamentals with C#/Recursion and Backtracking - Lab/06. 8 Queens Puzzle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._8_Queens_Puzzle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
