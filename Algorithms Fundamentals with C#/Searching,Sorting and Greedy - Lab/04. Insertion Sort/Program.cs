b087cdeb196a83f0508ade84104eaef4ef390cce
Polinadrumeva <polina9230@gmail.com>
2022-09-03 23:02:07 +0300

    add insertion and quick sort

--- Algorithms Fundamentals with C#/Searching,Sorting and Greedy - Lab/04. Insertion Sort/Program.cs
+++ Algorithms Fundamentals with C#/Searching,Sorting and Greedy - Lab/04. Insertion Sort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _04._Insertion_Sort
 {
@@ -6,7 +7,31 @@ namespace _04._Insertion_Sort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            InsertionSort(elements);
+
+            Console.WriteLine(String.Join(" ", elements));
+        }
+
+        private static void InsertionSort(int[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                var j = i;
+                while (j > 0 && elements[j-1] > elements[j])
+                {
+                    Swap(elements, j, j - 1);
+                    j--;
+                }
+            }
+
+        }
+
+        private static void Swap(int[] elements, int i, int min)
+        {
+            var temp = elements[i];
+            elements[i] = elements[min];
+            elements[min] = temp;
         }
     }
 }
--- Algorithms Fundamentals with C#/Searching,Sorting and Greedy - Lab/05. Quicksort/Program.cs
+++ Algorithms Fundamentals with C#/Searching,Sorting and Greedy - Lab/05. Quicksort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _05._Quicksort
 {
@@ -6,7 +7,52 @@ namespace _05._Quicksort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            QuickSort(elements, 0, elements.Length - 1);
+
+            Console.WriteLine(String.Join(" ", elements));
+        }
+
+        private static void QuickSort(int[] elements, int i, int numbersLength)
+        {
+            if (i >= numbersLength)
+            {
+                return;
+            }
+
+            var pivot = i;
+            var left = i + 1;
+            var right = numbersLength;
+
+            while (left <= right)
+            {
+                if (elements[left] > elements[pivot] && elements[right] < elements[pivot])
+                {
+                    Swap(elements, left, right);
+                }
+
+                if (elements[left] <= elements[pivot])
+                {
+                    left++;
+                }
+
+                if (elements[right] >= elements[pivot])
+                { 
+                right--;
+                }
+            }
+
+            Swap(elements, pivot, right);
+
+            QuickSort(elements, i, right - 1);
+            QuickSort(elements, right + 1, numbersLength);
+        }
+
+        private static void Swap(int[] elements, int i, int min)
+        {
+            var temp = elements[i];
+            elements[i] = elements[min];
+            elements[min] = temp;
         }
     }
 }
