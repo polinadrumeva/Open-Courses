e97d01e80a3cefc991538e5c8e27a696077749aa
Polinadrumeva <polina9230@gmail.com>
2022-09-03 13:23:45 +0300

    add algorithms searching

---
+++ Algorithms Fundamentals with C#/Searching,Sorting and Greedy - Lab/01. Binary Search/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Binary_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var index = int.Parse(Console.ReadLine());
+            Console.WriteLine(BinarySearch(elements, index));
+        }
+
+        public static int BinarySearch(int[] elements, int key)
+        {
+            var left = 0;
+            var right = elements.Length - 1;
+
+            while (left <= right)
+            { 
+                var mid = (left + right) / 2;
+                var midElement = elements[mid];
+                if (midElement == key)
+                {
+                    return mid;
+                }
+                else
+                {
+                    if (key > midElement)
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+
+            }
+
+            return -1;
+        }
+    }
+}
