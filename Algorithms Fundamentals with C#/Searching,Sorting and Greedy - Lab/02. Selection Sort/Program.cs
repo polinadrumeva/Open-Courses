dfbe4d1f2a144d03fa6671e36411b2ced063b325
Polinadrumeva <polina9230@gmail.com>
2022-09-03 13:52:09 +0300

    add selection and bubble sort

--- Algorithms Fundamentals with C#/Searching,Sorting and Greedy - Lab/02. Selection Sort/Program.cs
+++ Algorithms Fundamentals with C#/Searching,Sorting and Greedy - Lab/02. Selection Sort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _02._Selection_Sort
 {
@@ -6,7 +7,34 @@ namespace _02._Selection_Sort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            SelectionSort(elements);
+
+            Console.WriteLine(String.Join(" ", elements));
+        }
+
+        private static void SelectionSort(int[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                var min = i;
+                for (int j = i + 1; j < elements.Length; j++)
+                {
+                    if (elements[j] < elements[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                Swap(elements, i, min);
+            }
+        }
+
+        private static void Swap(int[] elements, int i, int min)
+        {
+            var temp = elements[i];
+            elements[i] = elements[min];
+            elements[min] = temp;
         }
     }
 }
--- Algorithms Fundamentals with C#/Searching,Sorting and Greedy - Lab/03. Bubble Sort/Program.cs
+++ Algorithms Fundamentals with C#/Searching,Sorting and Greedy - Lab/03. Bubble Sort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03._Bubble_Sort
 {
@@ -6,7 +7,40 @@ namespace _03._Bubble_Sort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            BubbleSort(elements);
+
+            Console.WriteLine(String.Join(" ", elements));
+        }
+
+        private static void BubbleSort(int[] elements)
+        {
+            var sortedCount = 0;
+            var isSorted = false;
+            while (!isSorted)
+            {
+                isSorted = true;
+                for (int i = 1; i < elements.Length -sortedCount; i++)
+                {
+                    var j = i - 1;
+                    if (elements[j] > elements[i])
+                    {
+                        Swap(elements, j, i);
+                        isSorted = false;
+                    }
+                }
+
+                sortedCount++;
+            }
+           
+            
+        }
+
+        private static void Swap(int[] elements, int i, int min)
+        {
+            var temp = elements[i];
+            elements[i] = elements[min];
+            elements[min] = temp;
         }
     }
 }
