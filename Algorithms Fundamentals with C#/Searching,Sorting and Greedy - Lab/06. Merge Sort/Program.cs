99bda4dc92ce163e325578a8e6ab3f96403768f4
Polinadrumeva <polina9230@gmail.com>
2022-09-04 17:50:38 +0300

    add merge sort

--- Algorithms Fundamentals with C#/Searching,Sorting and Greedy - Lab/06. Merge Sort/Program.cs
+++ Algorithms Fundamentals with C#/Searching,Sorting and Greedy - Lab/06. Merge Sort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _06._Merge_Sort
 {
@@ -6,7 +7,55 @@ namespace _06._Merge_Sort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var sorted = MergeSort(elements);
+
+            Console.WriteLine(String.Join(" ", sorted));
+        }
+
+        private static int[] MergeSort(int[] elements)
+        {
+            if (elements.Length <= 1)
+            {
+                return elements;
+            }
+
+            var left = elements.Take(elements.Length/2).ToArray();
+            var right = elements.Skip(elements.Length/2).ToArray();
+
+            return Merge(MergeSort(left), MergeSort(right));
+        }
+
+        private static int[] Merge(int[] left, int[] right)
+        {
+           var merged = new int[left.Length + right.Length];
+
+            var mergedIndex = 0;
+            var leftIndex = 0;
+            var rightIndex = 0;
+
+            while (leftIndex < left.Length && rightIndex < right.Length)
+            {
+                if (left[leftIndex] < right[rightIndex])
+                {
+                    merged[mergedIndex++] = left[leftIndex++];
+                }
+                else
+                {
+                    merged[mergedIndex++] = right[rightIndex++];
+                }
+            }
+            for (int i = leftIndex; i < left.Length; i++)
+            {
+                merged[mergedIndex++] = left[i];
+            }
+
+            for (int j = rightIndex; j < right.Length; j++)
+            {
+                merged[mergedIndex++] = right[j];
+            }
+            return merged;
         }
     }
 }
+
