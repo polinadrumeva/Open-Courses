c08fc1c85b79de769cf4453e655f837f96b0451d
Polinadrumeva <polina9230@gmail.com>
2022-12-11 18:18:19 +0200

    complete 03

--- Data Structures Fundamentals with C#/Linear Data Structures - Lab/Problem03.Queue/Queue.cs
+++ Data Structures Fundamentals with C#/Linear Data Structures - Lab/Problem03.Queue/Queue.cs
@@ -8,41 +8,102 @@
     {
         private class Node
         {
-            
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element)
+            {
+                this.Element = element;
+          }
         }
 
         private Node head;
 
-        public int Count => throw new NotImplementedException();
+        public int Count { get; private set; }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            if (this.head == null)
+            {
+                this.head = new Node(item);
+                this.Count++;
+                return;
+            }
+
+            var node = this.head;
+            while (node.Next != null)
+            {
+                node = node.Next;
+            }
+
+            node.Next = new Node(item);
+            this.Count++;
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var oldHead = this.head;
+            this.head = oldHead.Next;
+
+            this.Count--;
+            return oldHead.Element;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.head.Element;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+
+            if (this.Count == 0)
+            {
+                return false;
+            }
+
+            if (this.Count == 1)
+            {
+                return true;
+            }
+
+            var node = this.head;
+            while (node != null)
+            {
+                if (node.Element.Equals(item))
+                {
+                    return true;
+                }
+
+                node = node.Next;
+            }
+
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = this.head;
+            while (node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
         }
     }
 }
