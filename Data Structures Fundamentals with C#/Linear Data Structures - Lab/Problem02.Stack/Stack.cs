08a910103a05e55f1a918a604cc41680020c043b
Polinadrumeva <polina9230@gmail.com>
2022-12-11 17:57:52 +0200

    complete 02

--- Data Structures Fundamentals with C#/Linear Data Structures - Lab/Problem02.Stack/Stack.cs
+++ Data Structures Fundamentals with C#/Linear Data Structures - Lab/Problem02.Stack/Stack.cs
@@ -8,41 +8,89 @@
     {
         private class Node
         {
-            
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+              }
         }
 
         private Node top;
 
-        public int Count => throw new System.NotImplementedException();
+        public int Count { get; private set; }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            var node = new Node(item, this.top);
+            this.top = node;
+            this.Count++;
         }
 
         public T Pop()
         {
-            throw new System.NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var oldTop = this.top;
+            this.top = oldTop.Next;
+            this.Count--;
+            return oldTop.Element;
         }
 
         public T Peek()
         {
-            throw new System.NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.top.Element;
         }
 
         public bool Contains(T item)
         {
-            throw new System.NotImplementedException();
+            if (this.Count == 0)
+            {
+               return false;
+            }
+
+            if (this.Count == 1)
+            {
+                return true;
+            }
+
+            var node = this.top;
+            while (node != null)
+            {
+                if (node.Element.Equals(item))
+                {
+                    return true;
+                }
+                
+                node = node.Next;
+            }
+
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = this.top;
+            while (node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
+        { 
+            return GetEnumerator();
         }
     }
 }
