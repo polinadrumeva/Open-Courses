99dbf4415070d57b6efbec7802925c519267f4a5
Polinadrumeva <polina9230@gmail.com>
2022-12-13 14:00:32 +0200

    complete 04

--- Data Structures Fundamentals with C#/Linear Data Structures - Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ Data Structures Fundamentals with C#/Linear Data Structures - Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -7,7 +7,46 @@
     {
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+            if (parentheses.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            var stack = new Stack<char>(parentheses.Length / 2);
+
+            foreach (var current in parentheses)
+            {
+                char expected = default;
+                switch (current)
+                {
+                    case ')':
+                        expected = '(';
+                        break;
+                    case '}':
+                        expected = '{';
+                        break;
+                    case ']':
+                        expected = '[';
+                        break;
+                    default:
+                        stack.Push(current);
+                        break;
+                }
+
+                if (expected == default)
+                {
+                    continue;
+                }
+
+                if (stack.Pop() != expected)
+                {
+                    return false; 
+                }
+            }
+
+            
+            return stack.Count == 0;
+
         }
     }
 }
